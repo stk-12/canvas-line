4ea9e698da08d56ea0529a4907dfe223427c68e7
keisuke sato <corydoras121@gmail.com>
2022-09-04 17:01:08 +0900

    [update]sin波を複数のパラメータに反映

--- main.js
+++ main.js
@@ -59,7 +59,9 @@ function render(){
 
     // let par = ((passedTime % 1000) / 999); // 1秒ごとに0〜1を繰り返す
     // let angle = passedTime / 20 + (distanceX * 100); // １秒間に0.02増加する
-    let angle = passedTime / 20; // １秒間に0.02増加する
+    let angle = passedTime / 30; // １秒間に0.02増加する
+    let angle2 = passedTime / 80; // １秒間に0.02増加する
+    let angle3 = passedTime / 110; // １秒間に0.02増加する
 
     
 
@@ -69,37 +71,42 @@ function render(){
 
     for(let i = 0; i < num; i++){
       let radian = (angle + i) / 180 * Math.PI; //ラジアンに変換 i番目の角度の計算
+      let radian2 = (angle2 + i) / 180 * Math.PI;
+      let radian3 = (angle3 + i) / 180 * Math.PI;
   
       // fillRect(x, y, width, height)
       ctx.fillRect(
         interval * i,
-        (size.height * 0.4 - distanceY / 4) + ((size.height * 0.5) * ((Math.sin(radian * 3) + Math.cos(radian)) * 0.1)),
-        5 * distanceX + 3,
-        size.height * distanceY / 4 + 3
+        (size.height * 0.4) + ((size.height * 0.5) * ((Math.sin(radian * 3) + Math.cos(radian)) * 0.2)),
+        3,
+        size.height * 0.25 * Math.sin(radian3 * 1.8) * Math.cos(radian2 * 2) + 3
       );
     }
 
     for(let i = 0; i < num + 1; i++){
       let radian = (angle + i) / 180 * Math.PI;
+      let radian2 = (angle2 + i) / 180 * Math.PI;
   
       // fillRect(x, y, width, height)
       ctx.fillRect(
         interval * i - interval * 0.33333,
-        (size.height * 0.4 - distanceY / 4) + ((size.height * 0.5) * ((Math.sin(radian * 2) + Math.cos(radian * 3)) * 0.15)),
-        5 * distanceX + 3,
-        size.height * distanceY / 4 + 3
+        (size.height * 0.4) + ((size.height * 0.5) * ((Math.sin(radian2 * 2) + Math.cos(radian * 3)) * 0.08)),
+        3,
+        size.height * 0.25 * Math.sin(radian * 1.2) * Math.cos(radian2 * 3) + 3
       );
     }
 
     for(let i = 0; i < num + 1; i++){
       let radian = (angle + i) / 180 * Math.PI;
+      let radian2 = (angle2 + i) / 180 * Math.PI;
+      let radian3 = (angle3 + i) / 180 * Math.PI;
   
       // fillRect(x, y, width, height)
       ctx.fillRect(
         interval * i - interval * 0.66666,
-        (size.height * 0.4 - distanceY / 4) + ((size.height * 0.5) * ((Math.sin(radian) + Math.sin(radian * 3)) * 0.2)),
-        5 * distanceX + 3,
-        size.height * distanceY / 4 + 3
+        (size.height * 0.4) + ((size.height * 0.6) * ((Math.sin(radian3 * 1.2) + Math.sin(radian2 * 3)) * 0.2)),
+        3,
+        size.height * 0.25 * Math.sin(radian * 1.5) * Math.sin(radian * 2) + 3
       );
     }
     
